2c254cf6ad54b7922441593ad775c11c42584d20
Daniel porras <edward.porras@mail.escuelaing.edu.co>
2022-02-24 23:29:27 -0500

    react login

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1> Login </h1>
+          <TextField id="outlined-basic" label="User Or Email" variant="outlined" />
+          <br/>
+          <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+          />
+          <br/>
+          <br/>
+          <Button variant="contained">Contained</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
