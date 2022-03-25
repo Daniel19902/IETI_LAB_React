9d0f80c0029d6800cfb4686ca193b460f3e68bdd
Daniel porras <edward.porras@mail.escuelaing.edu.co>
2022-03-24 22:43:00 -0500

    lab-state-management-pwa

--- src/App.js
+++ src/App.js
@@ -5,24 +5,41 @@ import Connect from "./components/Connect";
 import Loby from "./components/Loby"
 import React from "react";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { useReducer } from "react"
+import { themeReducer } from './utils';
+import { initialState } from './utils';
+import { ThemeContext } from './ThemeContext'
+import Button from '@mui/material/Button';
+import { useContext } from "react";
 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-            <Routes>
-                <Route path="/Login" element={<Longin></Longin>} />
-            </Routes>
-            <Routes>
-                <Route path="/Conectar/:email/:password" element={<Connect></Connect>}> hola </Route>
-            </Routes>
-            <Routes>
-                <Route path="/Loby" element={<Loby></Loby>}> hola </Route>
-            </Routes>
+    const [state, dispatch] = useReducer(themeReducer, initialState);
 
-        </div>
-      </Router>
-  );
+    return (
+        <ThemeContext.Provider  value={{ state, dispatch }}>
+            <Router>
+                <div className="App">
+                    <Button onClick={() => {
+                        if (state.isDarkMode) {
+                            dispatch("SET_LIGHT_MODE");
+                        } else {
+                            dispatch("SET_DARK_MODE");
+                        }
+                    }}>Change Theme </Button>
+                    <Routes>    
+                        <Route path="/Login" element={<Longin></Longin>} />
+                    </Routes>
+                    <Routes>
+                        <Route path="/Conectar/:email/:password" element={<Connect></Connect>}> hola </Route>
+                    </Routes>
+                    <Routes>
+                        <Route path="/Loby" element={<Loby></Loby>}> hola </Route>
+                    </Routes>
+
+                </div>
+            </Router>
+        </ThemeContext.Provider>
+    );
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,14 @@
 import '../App.css'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import React, { Component, useState, useEffect } from "react";
-import Connect from "./Connect";
+import React, {  useState } from "react";
+import '../index.css'
+
 import { useNavigate } from "react-router-dom";
-
+import { useContext } from "react";
+import { ThemeContext } from "../ThemeContext";
 function Longin () {
-
+    const { state  } = useContext(ThemeContext);
     const [ credenciales, setCredenciales] = useState({
         email : null,
         password : null
@@ -16,12 +18,16 @@ function Longin () {
     const navigate = useNavigate();
 
     return(
-        <div className="container">
+        <div className="container ">
+            <div className={`button-${state.isDarkMode ? "dark" : "light"}`}>
             <h1> Login </h1>
-            <TextField id="outlined-basic" label="User Or Email" variant="outlined" onChange={event => setCredenciales({email: event.target.value,
-            password: credenciales.password})}/>
+            <TextField
+            className={`button-${state.isDarkMode ? "light" : "light"}`} 
+            id="outlined-basic" label="User Or Email" variant="outlined" onChange={event => setCredenciales({email: event.target.value,
+            password: credenciales.password}) } />
             <br/>
             <TextField
+                className={`button-${state.isDarkMode ? "light" : "light"}`}
                 id="outlined-password-input"
                 label="Password"
                 type="password"
@@ -31,7 +37,9 @@ function Longin () {
             <br/>
 
             <br/>
+            
             <Button variant="contained" onClick={() => navigate('/Conectar/'+ credenciales.email+'/'+credenciales.password ,{replace: true})}>Sing In</Button>
+            </div>
         </div>
     )
 }
